4f140d385d8a736c6afa4ff1af70922b9535c81b
lee.an <lee.an@ITMACOSLEEAN.local>
2024-06-05 15:01:15 +0800

    Add some example and update Readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # flutter_micro_extension
 
 A Flutter extension micro service for public use.
+
+Include this package in your flutter project to use all the extensions.
+
+Local - Save in some local folder
+  dependencies:
+    library_name:
+    path: /path/to/library_name
+
+flutter_micro_extension:
+  git:
+    url: https://github.com/leean912/flutter_extension
+
+Or to target exact branch
+  flutter_micro_extension:
+    git:
+      url: https://github.com/leean912/flutter_extension
+      ref: main # branch name
+
+Or to target exact commit
+  flutter_micro_extension:
+    git:
+      url: https://github.com/leean912/flutter_extension
+      ref: ea12e41 # commit hash
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_micro_extension/build_context_ext.dart';
+import 'package:flutter_micro_extension/num_ext.dart';
+import 'package:flutter_micro_extension/widget_ext.dart';
 
 // In main.dart will show how to use all extension example
 
@@ -15,10 +17,18 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            'Hello World!',
-            // How to use context extension
-            style: TextStyle(color: context.primaryColor),
+          child: Column(
+            children: [
+              // How to use num extension to create sizedbox
+              20.heightBox,
+              30.widthBox,
+              // How to use widget extension to extend your widget
+              Text(
+                'Hello World!',
+                // How to use context extension
+                style: TextStyle(color: context.primaryColor),
+              ).box(height: 50).center(), // Always from left to right
+            ],
           ),
         ),
       ),
